8812060678778f6d241ae51be8ea82c9541775bd
KestralDEV <83971549+KestralDex@users.noreply.github.com>
2025-07-11 20:57:58 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+operator = input('Enter the operator (+, -, *, /, %, //, **): ')
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+if (operator == '+'):
+    print(num1 + num2)
+elif (operator == '-'):
+    print(num1 - num2)
+elif (operator == '*'):
+    print(num1 * num2)
+elif (operator == '/'):
+    print(num1 / num2)
+elif (operator == '%'):
+    print(num1 % num2)
+elif (operator == '//'):
+    print(num1 // num2)
+elif (operator == '**'):
+    print(num1 ** num2)
+else:
+    print('Invalid operator')
